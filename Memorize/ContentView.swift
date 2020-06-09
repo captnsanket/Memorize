f8c748a733d857e5d4a1845851211d352c79f4d3
Subha <subha.samanta@justdial.com>
2020-06-09 08:55:02 +0530

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Subha Sanket on 09/06/20.
+//  Copyright © 2020 Subha Sanket. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
